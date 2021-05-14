10191d016d73dcc14e7b5575b835397ecc8885f8
youmeSon <rosydarling@naver.com>
2021-05-14 15:37:49 +0100

    Add arrow up

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 "use strict";
 
 const navbar = document.querySelector(".navbar");
+const main = document.querySelector("#main");
+const mainHeight = main.getBoundingClientRect().height;
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
+// Show arrow up
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > mainHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
+
+//  Handle arrow up button
+arrowUp.addEventListener("click", () => {
+  scrollIntoView("#main");
+});
