6258ec452767e7ba203a8393b62d7208027ea682
youmeSon <rosydarling@naver.com>
2021-05-15 11:18:17 +0100

    JS: Show more + Contact us + Submit button

--- main.js
+++ main.js
@@ -4,10 +4,35 @@ const navbar = document.querySelector(".navbar");
 const main = document.querySelector("#main");
 const mainHeight = main.getBoundingClientRect().height;
 
+// Scroll to section
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+// Show more toggle button
+const showMoreBtn = document.querySelector(".button__more");
+const showMore = document.querySelector(".show__more");
+showMoreBtn.addEventListener("click", () => {
+  const buttonText = showMoreBtn.innerText;
+  showMore.classList.toggle("hide");
+  showMoreBtn.innerText = buttonText == "Hide" ? "Show more" : "Hide";
+});
+
+// Contact button
+const contactBtn = document.querySelector(".button__contact");
+contactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+// Submit button
+const submitBtn = document.querySelector(".submit__btn");
+const submitMessage = document.querySelector(".submit__message");
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  submitMessage.classList.remove("hide");
+});
+
 // Show arrow up
 const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
